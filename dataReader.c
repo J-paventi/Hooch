0668b0937280d865c517a3f44ba2c237e6283b34
Josh Horsley <jhorsley3027@conestogac.on.ca>
2025-03-09 16:02:52 -0400

    added code to check if a memory queue and enough shared memory exists. if not it creates them

--- dataReader.c
+++ dataReader.c
@@ -32,6 +32,7 @@ int main(void)
     key_t shmKey;
     int msgQueueID;
     int shmID;
+    MasterList* masterList;
 
     msgKey = ftok(".", MSG_KEY);
     shmKey = ftok(".", SHM_KEY);
@@ -39,7 +40,40 @@ int main(void)
     if (msgKey == -1 || shmKey == -1)
     {
         perror("Failed to genrate keys"); //remove later
-        exit(EXIT_FAILURE)
+        exit(EXIT_FAILURE);
+    }
+
+    // check if the message queue exists...
+    if((msgQueueID = msgget(msgKey, 0)) == -1)
+    {
+        // message queue doesn't exist so create it
+        msgQueueID = msgget(msgKey, (IPC_CREAT | 0660 ));
+        if(msgQueueID == -1)
+        {
+            perror("message queue creation failed"); // remove later
+        }else
+        {
+            printf("Message queue created with ID: %d \n", msgQueueID); // remove later
+        }
+    }
+
+    //Check if enough shared memory exists and potentially allocates it
+    if((shmID = shmget(shmKey, sizeof(MasterList),0)) == -1)
+    {
+        // memory doesnt exist - create it
+        shmID = shmget(shmKey, sizeof(MasterList), (IPC_CREAT |0660));
+        if (shmID == -1)
+        {
+            perror("Shared memory creation failed"); //remove later
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    masterList = (MasterList*)shmat(shmID, NULL, 0):
+    if (masterList == (void*)-1)
+    {
+        perror("failed to attach shared memory";)
+        exit(EXIT_FAILURE);
     }
 }
 
