e18107095612fda27d61e9e195317851979e0f5e
Josh Horsley <jhorsley3027@conestogac.on.ca>
2025-03-09 15:05:44 -0400

    added the includes for the dataReader and added the master list struct

--- dataReader.c
+++ dataReader.c
@@ -1,2 +1,33 @@
-// This is the function that should contain all the logic for the 
-// reading of the data done by the program
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/msg.h>
+#include <time.h> 
+#include <string.h>
+
+#define MAX_DC_ROLES 10
+#define SHM_KEY 16535
+#define MSG_KEY 1234 //temporary key number
+
+typedef struct 
+{
+    pid_t dcProcessID;
+    time_t lastTimeHeardFrom;
+
+} DCInfo;
+
+typedef struct
+{
+    int msgQueueID;
+    intnumverOfDCs;
+    DCInfo dc[MAX_DC_ROLES];
+
+} MasterList;
+
+int main(void)
+{
+
+}
+
