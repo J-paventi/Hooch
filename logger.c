6873d337c8d7e66ee3d3dc0657aee43363db0014
Josh Horsley <jhorsley3027@conestogac.on.ca>
2025-03-09 21:11:35 -0400

    began splitting the data reader up to be more modular

---
+++ logger.c
@@ -0,0 +1,17 @@
+#include "logger.h"
+#include "data_structures.h"
+#include <stdio.h>
+#include <time.h>
+
+void logMessage(const char* message) {
+    FILE* logFile = fopen(LOG_FILE, "a");
+    if (logFile) {
+        time_t now = time(NULL);
+        struct tm* tm_info = localtime(&now);
+        char timestamp[20];
+        strftime(timestamp, sizeof(timestamp), "%Y-%m-%d %H:%M:%S", tm_info);
+
+        fprintf(logFile, "[%s] : %s\n", timestamp, message);
+        fclose(logFile);
+    }
+}
